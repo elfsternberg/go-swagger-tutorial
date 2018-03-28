a27ae64d92a82f465ec6259209a1279e6585d0c0
Elf M. Sternberg <elf.sternberg@gmail.com>
2018-03-28 12:23:05 -0700

    Adding all features provided by swagger generate and dep init.
    At thing point, we've configued the swagger file and run 'dep init', which
    goes out and finds all the libraries in use by the swagger runtime, downloads
    them, and incorporates them into the build process.
    
    We can run `go build ./cmd/timeofday-server` and the server will show
    up in our project's base directory.  We can run the server:
    `./timeofday-server --port=8020`, and then ping the endpoints:
    
    curl http://localhost:8020
    {"code":404,"message":"path / was not found"}
    
    curl http://localhost:8020/timeofday/v1/
    "operation .ClockGet has not yet been implemented"
    
    Note that one returns an object, and the other returns a string.
    Both are valid JSON objects.

---
+++ restapi/configure_timeofday.go
@@ -0,0 +1,71 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	errors "github.com/go-openapi/errors"
+	runtime "github.com/go-openapi/runtime"
+	middleware "github.com/go-openapi/runtime/middleware"
+	graceful "github.com/tylerb/graceful"
+
+	"github.com/elfsternberg/timeofday/restapi/operations"
+)
+
+//go:generate swagger generate server --target .. --name  --spec ../swagger.yml
+
+func configureFlags(api *operations.TimeofdayAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.TimeofdayAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	api.ClockGetHandler = operations.ClockGetHandlerFunc(func(params operations.ClockGetParams) middleware.Responder {
+		return middleware.NotImplemented("operation .ClockGet has not yet been implemented")
+	})
+	api.ClockPostHandler = operations.ClockPostHandlerFunc(func(params operations.ClockPostParams) middleware.Responder {
+		return middleware.NotImplemented("operation .ClockPost has not yet been implemented")
+	})
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix"
+func configureServer(s *graceful.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
